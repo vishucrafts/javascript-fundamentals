e2869ed1ff1a3f183fce82cae0114aa0e8c9aea5
vishucrafts <contact@vishu.io>
2023-07-31 12:42:24 +0530

    01-functions

---
+++ src/01-functions.js
@@ -0,0 +1,14 @@
+// Normal function
+// function add(x, y) {
+// 	return x + y
+// }
+
+// Arrow function
+// const add = (x, y) => {
+// 	return x + y
+// }
+
+// Arrow function with implicit return
+// const add = (x, y) => x + y
+
+console.log(add(1, 2))
